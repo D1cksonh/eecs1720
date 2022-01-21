bf6147c5a4b095a44262e8384ce41e4475fc6a2e
D1cksonh <90432572+D1cksonh@users.noreply.github.com>
2022-01-20 21:45:00 -0500

    Jan 20th 2022 log

---
+++ 100 Days of Code/Jan 20th 2022/content.js
@@ -0,0 +1,18 @@
+//The code exicutes after a webpage loaads, it can effect and manipulate the contents of the webpage of a website
+
+console.log("Chrome exntention go");
+
+chrome.runtime.onMessage.addListener(gotMessage);
+
+function gotMessage(message, sender, sendResponse){
+    console.log(message.txt);
+    
+    if(message.txt == "hello") { // if button extention is pressed, all of the paragraphs turn to purple 
+        let paragraphs = document.getElementsByTagName('p');
+        
+        for (elt of paragraphs) {
+        elt.style['background-color'] = '#FF00FF';
+        }
+    }
+}
+
